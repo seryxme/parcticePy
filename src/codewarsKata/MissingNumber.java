db70cc5960fcaa7aea14e681f9bf36a9ab870b04
seryxme <coolbams24@yahoo.com>
2022-06-29 01:09:25 +0100

    First commit. #100DaysOfCode

--- src/codewarsKata/MissingNumber.java
+++ src/codewarsKata/MissingNumber.java
@@ -11,18 +11,33 @@ public class MissingNumber {
     public static int findMissing(int[] numbers) {
         int[] differences = new int[numbers.length - 1];
 
+
         for (int i = 0; i < numbers.length - 1; i++) {
-            differences[i] = numbers[i + 1] - numbers[i];
+            if (numbers[i + 1] - numbers[i] < 0) differences[i] = numbers[i] - numbers[i + 1];
+            else differences[i] = numbers[i + 1] - numbers[i];
         }
 
         int missingNum = numbers[numbers.length - 1] + differences[differences.length - 1];
 
         for (int i = 0; i < differences.length - 1; i++) {
-            if (differences[i + 1] - differences[i] != 0) {
-                if (differences[i] > differences[i + 1]) missingNum = numbers[i] + differences[i + 1];
-                else missingNum = numbers[i + 1] + differences[i];
+
+            if (differences[i] > differences[i + 1] && numbers[i] > numbers[i + 1]) {
+                missingNum = numbers[i] - differences[i + 1];
                 break;
             }
+            else if (differences[i] < differences[i + 1] && numbers[i] > numbers[i + 1]) {
+                missingNum = numbers[i + 1] - differences[i];
+                break;
+            }
+            else if (differences[i] > differences[i + 1]) {
+                missingNum = numbers[i] + differences[i + 1];
+                break;
+            }
+            else if (differences[i + 1] - differences[i] != 0) {
+                missingNum = numbers[i + 1] + differences[i];
+                break;
+            }
+
         }
         return missingNum;
     }
